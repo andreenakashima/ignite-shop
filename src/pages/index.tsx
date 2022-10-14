1a22e0a131524f38d28241857b84547721b3e427
andreenakashima <andreenakashima@gmail.com>
2022-10-14 15:54:50 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+	backgroundColor: "$rocketseat",
+	borderRadius: 4,
+	padding: "4px 8px",
+	border: 0,
+	span: {
+		fontWeight: "bold",
+	},
+	"&:hover": {
+		filter: "brightness(0.8)",
+	},
+});
+
 export default function Home() {
-	return <h1>Hello World</h1>;
+	return (
+		<Button>
+			<span>teste</span>Enviar
+		</Button>
+	);
 }
