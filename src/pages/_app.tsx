9c9bae8ee282b829ada40f90a470f66d3b705188
andreenakashima <andreenakashima@gmail.com>
2022-10-16 17:05:01 -0300

    Estrutura da Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { globalStyles } from "../styles/global";
 import LogoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 
+import Image from "next/future/image";
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Container>
 			<Header>
-				<img src={LogoImg.src} alt="" />
+				<Image src={LogoImg} alt="" />
 			</Header>
 			<Component {...pageProps} />
 		</Container>
