ae4cb8fee92cf849cb5c88e4f0b73f04f8efeb87
andreenakashima <andreenakashima@gmail.com>
2022-10-17 20:35:33 -0300

    Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Image from "next/future/image";
-import { HomeContainer, Product } from "../styles/pages/home";
 import Stripe from "stripe";
+
+import { HomeContainer, Product } from "../styles/pages/home";
 import { stripe } from "../lib/stripe";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 
 import { useKeenSlider } from "keen-slider/react";
 
@@ -29,14 +31,16 @@ export default function Home({ products }: HomeProps) {
 		<HomeContainer ref={sliderRef} className="keen-slider">
 			{products.map((product) => {
 				return (
-					<Product key={product.id} className="keen-slider__slide">
-						<Image src={product.imageUrl} width={520} height={480} alt="" />
+					<Link href={`/product/${product.id}`} key={product.id}>
+						<Product className="keen-slider__slide">
+							<Image src={product.imageUrl} width={520} height={480} alt="" />
 
-						<footer>
-							<strong>{product.name}</strong>
-							<span>{product.price}</span>
-						</footer>
-					</Product>
+							<footer>
+								<strong>{product.name}</strong>
+								<span>{product.price}</span>
+							</footer>
+						</Product>
+					</Link>
 				);
 			})}
 		</HomeContainer>
