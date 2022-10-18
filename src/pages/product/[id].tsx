064a85aa61a74478167bb3d97c2cc7701eb56d86
andreenakashima <andreenakashima@gmail.com>
2022-10-18 15:39:21 -0300

    SSG com parametro dinamico

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/future/image";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
@@ -36,6 +36,13 @@ export default function Product({ product }: ProductProps) {
 		</ProductContainer>
 	);
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: [{ params: { id: "prod_MdEKEPwEASzUNS" } }],
+		fallback: false,
+	};
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
 	params,
