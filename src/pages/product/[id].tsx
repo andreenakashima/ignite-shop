aa9d38292ba34033666b86f7cd8fb8b0799779fe
andreenakashima <andreenakashima@gmail.com>
2022-10-18 14:21:41 -0300

    Estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
 import { useRouter } from "next/router";
+import {
+	ImageContainer,
+	ProductContainer,
+	ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
 	const { query } = useRouter();
-	return <h1>Product {JSON.stringify(query)}</h1>;
+	return (
+		<ProductContainer>
+			<ImageContainer></ImageContainer>
+
+			<ProductDetails>
+				<h1>Camiseta X</h1>
+				<span>R$ 79,90</span>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea
+					non, est dolor eos saepe omnis, ratione consequatur dolores itaque
+					voluptate corrupti alias, quod atque animi excepturi ducimus quibusdam
+					modi!
+				</p>
+
+				<button>Comprar agora</button>
+			</ProductDetails>
+		</ProductContainer>
+	);
 }
