a1d20ea79ee3491c8e967ea4eff0b7fc46c46ae6
andreenakashima <andreenakashima@gmail.com>
2022-10-20 10:32:30 -0300

    Redirecionamento no SSR

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -12,6 +12,7 @@ interface SuccessProps {
 		imageUrl: string;
 	};
 }
+
 export default function Success({ customerName, product }: SuccessProps) {
 	return (
 		<SuccessContainer>
@@ -32,6 +33,15 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	if (!query.session_id) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+
 	const sessionId = String(query.session_id);
 
 	const session = await stripe.checkout.sessions.retrieve(sessionId, {
